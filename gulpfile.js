9973305cc6f62b081c82f3fe68cd4639a1307143
Vasudevan PALANI <vasu@Vasudevans-MacBook-Pro.local>
2017-04-15 19:52:29 -0400

    Fixed the gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,22 @@ var c = require('colors');
 var targz = require('tar.gz');
 var fs = require('fs');
 
-var repo = "./repo";
-var components = "./components";
-
-
 gulp.task('package',function(){
-  if(argv.name == undefined || argv.version == undefined){
-    console.log(c.red("Please enter a valid package name and version"));
+  if(!argv.name || !argv.version || !argv.repo || !argv.base){
+    console.log(c.red("Usage : gulp package --name <package name>\
+     --version <package version>\
+     --repo <repository path>\
+     --base <base directory where the package is currently as directory>"));
   }
   else {
     console.log(c.green("PACKAGE NAME : ",argv.name));
     console.log(c.green("PACKAGE VERSION : ",argv.version));
+    console.log(c.green("REPO : ",argv.repo));
+    console.log(c.green("BASE : ",argv.base));
 
     // Create all streams that we need
-    var read = targz().createReadStream(components+"/"+argv.name);
-    var write = fs.createWriteStream(repo+"/"+argv.name+"-"+argv.version+'.tar.gz');
+    var read = targz().createReadStream(argv.base+"/"+argv.name);
+    var write = fs.createWriteStream(argv.repo+"/"+argv.name+"-"+argv.version+'.tar.gz');
 
     // Let the magic happen
     read.pipe(write);
